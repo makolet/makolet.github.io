e7c6c1656cee76e59c2fa2c01773a188ab7511c0
makolet <makolettamiradler@gmail.com>
2017-03-31 15:44:41 +0300

    Add files via upload

--- app/makolet-list/makolet-list.component.js
+++ app/makolet-list/makolet-list.component.js
@@ -3,18 +3,7 @@ angular.
   component('makoletList', {
     templateUrl: 'app/makolet-list/makolet-list.template.html',
     controller: ['sharedProperties', function makoletListController(sharedProperties) {
-        this.makoletList = findGetParameter();/*[
-          {
-            name: 'Nexus S'
-            
-          }, {
-            name: 'Motorola XO'
-          
-          }, {
-            name: 'MOTOROLA XO'
-          
-          }
-        ];*/
+        this.makoletList = findGetParameter();
         sharedProperties.field = this.makoletList;
       }
     ]
@@ -22,13 +11,12 @@ angular.
   
   
 function findGetParameter() {
-  var result = [],
-  tmp = [];
-  var items = location.search.substr(1).split("&");
+  var result = [];
+  /*var items = location.search.substr(1).split("&");
   for (var index = 0; index < items.length; index++) {
     var item = {name: decodeURIComponent(items[index])};
     result.push(item);
-  }
+  }*/
   console.log(result);
   return result;
 }
