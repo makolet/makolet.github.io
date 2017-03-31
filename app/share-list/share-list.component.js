06d1335086090072c3a8cd0f93c2d0093e4810da
makolet <makolettamiradler@gmail.com>
2017-03-31 16:05:44 +0300

    Add files via upload

--- app/share-list/share-list.component.js
+++ app/share-list/share-list.component.js
@@ -3,7 +3,26 @@ angular.
   component('shareList', {
     templateUrl: 'app/share-list/share-list.template.html',
     controller: ['sharedProperties', function shareListController(sharedProperties) {
-        this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent); 
+        this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
+        this.makoletList = sharedProperties.field; 
+        var self = this;
+        this.getUrl = function () {
+          return encodeQueryData(self.makoletList);
+        } 
       }
     ]
   });
+  
+  
+  
+function encodeQueryData(data) {
+  
+  var arrayLength = data.length;
+  for (var i = 0; i < arrayLength; i++) {
+    alert(data[i]);
+    var itemObj = data[i];
+    ret.push(encodeURIComponent(itemObj.name));
+  }
+  
+  return ret.join('&');
+}
