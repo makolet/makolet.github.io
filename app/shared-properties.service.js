a1ac9340c3b34b5e441d294e3745c329dc3ee07f
makolet <makolettamiradler@gmail.com>
2017-03-31 16:37:11 +0300

    Add files via upload

--- app/shared-properties.service.js
+++ app/shared-properties.service.js
@@ -4,3 +4,8 @@ var myApp = angular.module('makoletApp')
 myApp.factory('sharedProperties', function(){
   return { field: [] };
 });
+
+myApp.config(function($compileProvider){
+   //other configuration code here
+   $compileProvider.aHrefSanitizationWhitelist(/^\s*(whatsapp):/);
+})
