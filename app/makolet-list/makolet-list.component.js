2642df05e93f7394de5c5d8a97607f356bd431b7
makolet <makolettamiradler@gmail.com>
2017-03-31 15:50:59 +0300

    Add files via upload

--- app/makolet-list/makolet-list.component.js
+++ app/makolet-list/makolet-list.component.js
@@ -9,14 +9,38 @@ angular.
     ]
   });
   
-  
 function findGetParameter() {
   var result = [];
-  /*var items = location.search.substr(1).split("&");
+  var items = location.search.substr(1).split("&");
   for (var index = 0; index < items.length; index++) {
-    var item = {name: decodeURIComponent(items[index])};
-    result.push(item);
-  }*/
+    var item = decodeURIComponent(items[index]);
+    if (!isEmpty(item))
+    {
+      var itemObj = {name: item};
+      result.push(itemObj);
+    }
+  }
   console.log(result);
   return result;
+}
+
+function isEmpty(obj) 
+{
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) 
+    {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
 }
